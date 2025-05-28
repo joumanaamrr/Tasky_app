fde3045f8c1be08c5c4463ccc07d0116a8f22744
Jomana <joumanaamr202122978@gmail.com>
2025-05-28 20:37:50 +0300

    first edits

---
+++ lib/LoginScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+
+class LoginScreen extends StatelessWidget{
+  static const String Routname='LoginScreen';
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+   return Scaffold(
+   body: Column(
+     children: [
+     // SvgPicture.asset('assets/images/check.svg',
+     // width: 100,
+     // height: 100,)
+
+     ],
+   )
+   );
+  }
+  
+
+}
